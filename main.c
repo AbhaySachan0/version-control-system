02e0a8ade596eb17b2a37ad54f2f3cbabe5ad218
AbhaySachan0 <abhaysach05@gmail.com>
2026-02-08 16:32:09 +0530

    added initialize repo function

--- main.c
+++ main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h> // access()
+#include <sys/stat.h>
+
+
+void init_repo() {
+    
+    if(access(".myvc",F_OK)==0){
+        printf("Repo already exists\n");
+        return;
+    }
+    
+    if(mkdir(".myvc",0755) == -1){
+        perror("mkdir");
+        return;
+    }
+    printf("Initialized empty repository\n");
+}
 
 int main(int argc, char* argv[]) {
 
     if(argc < 2) {
-        printf("No Arguments...\n");
-        // return 1;
+        fprintf(stderr,"Usage: %s No commands\n",argv[0]);
+        return 1;
     }
     
     if(strcmp(argv[1],"init") == 0) {
-
-        printf("Init recognised\n");
-    }
-
+        init_repo();
+    } 
+    
     return 0;
 }
