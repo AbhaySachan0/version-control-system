8cad81cb2e42f1375908ae95aeeb8b79f1371641
AbhaySachan0 <abhaysach05@gmail.com>
2026-02-09 15:25:17 +0530

    added paths and init

--- main.c
+++ main.c
@@ -1,33 +1,106 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h> // access()
-#include <sys/stat.h>
+#include <sys/stat.h> // mkdir()
+#include <linux/limits.h>
 
 
-void init_repo() {
+
+typedef struct{
+    char* repo_root; // repo root path
+    char* git_dir; // .git path
+    char* object_dir;    // .git/objects
+    char* ref_dir;      //.git/ref
+    char* head_dir;
+    char* head_file; //.git/HEAD
+                
+    char* index_dir;    //.git/index
+}Paths;
+
+static char* allocate_path(void) {
+    char* p = malloc(4096);
+
+    if(!p){
+        perror("malloc");
+        exit(1);
+    }
+    return p;
+}
+
+void initialize_path(Paths *p, const char *root) {
+    p->repo_root = allocate_path();
+    p->git_dir = allocate_path();
+    p->object_dir = allocate_path();
+    p->ref_dir = allocate_path();
+    p->head_dir = allocate_path();
+    p->head_file = allocate_path();
+    p->index_dir = allocate_path();
+    
+    
+    snprintf(p->repo_root,4096,"%s",root);
+    snprintf(p->git_dir,4096,"%s/.myvc",root);
+    snprintf(p->object_dir,4096,"%s/.myvc/objects",root);
+    snprintf(p->ref_dir,4096,"%s/.myvc/refs",root);
+    snprintf(p->head_dir,4096,"%s/.myvc/refs/heads",root);
+    snprintf(p->head_file,4096,"%s/.myvc/HEAD",root);
+    snprintf(p->index_dir,4096,"%s/.myvc/index",root);
+
+    
+}
+
+void make_dir(const char *path) {
+    if(mkdir(path,0755) == -1){
+        perror("mkdir");
+        return;
+        
+    }
+    printf("%s\n",path);
+}
+
+
+void init_repo(Paths *p) {
     
     if(access(".myvc",F_OK)==0){
         printf("Repo already exists\n");
         return;
     }
+
+    make_dir(p->git_dir);
+    make_dir(p->object_dir);
+    make_dir(p->index_dir);
+    make_dir(p->ref_dir);
+    make_dir(p->head_dir);
     
-    if(mkdir(".myvc",0755) == -1){
-        perror("mkdir");
-        return;
-    }
+    
+    
     printf("Initialized empty repository\n");
 }
 
 int main(int argc, char* argv[]) {
 
+    char resolved_path[PATH_MAX];
+    realpath(".",resolved_path);
+    // printf("%s\n",resolved_path);
+
+    Paths p;
+
+    initialize_path(&p,resolved_path);
+    // printf("%s\n",p.git_dir);
+
+
+
     if(argc < 2) {
-        fprintf(stderr,"Usage: %s No commands\n",argv[0]);
+        fprintf(stderr,"Usage: %s No arguments\n",argv[0]);
         return 1;
-    }
+    }    
     
     if(strcmp(argv[1],"init") == 0) {
-        init_repo();
-    } 
+        init_repo(&p);
+    }
     
+    struct stat st;
+
     return 0;
 }
