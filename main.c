ab9da173998bf3cde2cae346683aa1c6ac0e736c
AbhaySachan0 <abhaysach05@gmail.com>
2026-02-11 23:24:42 +0530

    added blob object

--- main.c
+++ main.c
@@ -79,7 +79,7 @@ void init_repo(Paths *p) {
     printf("Initialized empty repository\n");
 }
 
-int read_file(const char* path, char **content, size_t *file_size){
+int read_file(const char *path, char **content, size_t *file_size){
     struct stat st;
     if(stat(path,&st)==-1){
         perror("file");
@@ -92,16 +92,16 @@ int read_file(const char* path, char **content, size_t *file_size){
 
     if(fd==-1) {
         perror("file");
-        close(fd);
         return -1;
     }
 
     char* buffer = malloc(size);
     size_t total = 0;
     while(total <size) {
-        size_t n = read(fd, buffer+total, size-total);
+        ssize_t n = read(fd, buffer+total, size-total);
         if(n<=0) {
             perror("read");
+            free(buffer);
             close(fd);
             return -1;
         }
@@ -124,6 +124,81 @@ void hash_content(char* hex,char *content, size_t size) {
     }
     hex[SHA_DIGEST_LENGTH*2] = '\0';
 
+    // printf("%s\n",hex);
+}
+
+void store_object(char* hex, char* content, size_t size) {
+    char dir[3];
+    strncpy(dir,hex,2);
+    dir[2] = '\0';
+
+    char dir_path[PATH_MAX];
+
+    snprintf(dir_path,PATH_MAX,".myvc/objects/%s",dir);
+    printf("store objects\n");
+
+    if(access(dir_path,F_OK)==-1) { // folder doesn't exist
+        if(mkdir(dir_path,0755)==-1) {
+            perror("mkdir");
+            return;
+        }
+    }
+    
+    printf("objects\n");
+    
+    
+    const char* file = hex +2;
+    char obj_path[PATH_MAX];
+
+    // snprintf(obj_path,sizeof(obj_path),"%s/%s",dir_path,file);
+    int n = snprintf(obj_path, sizeof(obj_path),".myvc/objects/%c%c/%s",hex[0], hex[1], hex + 2); 
+
+    if(access(obj_path,F_OK)==0) {
+        return;
+    }
+    int fd = open(obj_path,O_WRONLY | O_CREAT | O_EXCL,0644);
+    if(fd==-1) {
+        perror("open");
+        return;
+    }
+    size_t total = 0;
+
+    while(total<size) {
+        ssize_t n = write(fd,content+total,size-total);
+        if(n<=0) {
+            perror("write");
+            close(fd);
+            return;
+        }
+        total += n;
+    }
+    close(fd);
+    printf("store objects\n");
+
+}
+
+void blob_object(const char* content, size_t content_size, char *hex, char** out_buf, size_t *out_size) {
+    char header[100];
+    int header_len = snprintf(header, sizeof(header), "blob %zu", content_size);
+
+    size_t total = content_size + header_len +1;
+
+    char* buffer = malloc(total);
+    if(!buffer){
+        perror("malloc");
+        exit(1);
+    }
+
+    memcpy(buffer,header,header_len);
+    buffer[header_len] = '\0';
+    memcpy(buffer+header_len+1, content, content_size);
+
+    hash_content(hex,buffer,total);
+
+    *out_buf = buffer;
+    *out_size = total;
+
+    free(buffer);
 }
 
 void add_file(const char* path) {
@@ -135,12 +210,19 @@ void add_file(const char* path) {
     }
     // hash the content
     char hex[41];
-    hash_content(&hex,content,size);
-    //store content
 
+    char *blob_buf;
+    size_t blob_size;
+
+    blob_object(content, size, hex, &blob_buf, &blob_size);
+
+    //store content
+    store_object(hex,content,size);
+    
     
     //update index
-
+    
+    free(content); 
 }
 
 void process_path(const char* path) {
@@ -159,7 +241,8 @@ void process_path(const char* path) {
     if(S_ISREG(st.st_mode)){
         add_file(path);
     } else if (S_ISDIR(st.st_mode)) {
-        add_directory(path);
+        // add_directory(path);
+        printf("Directory\n");
     } else {
         printf("INVALID PATH");
     }
@@ -178,7 +261,6 @@ void add(int argc,char* argv[]){
         process_path(argv[i]);
     }
 
-    
 }
 
 
@@ -205,7 +287,5 @@ int main(int argc, char* argv[]) {
         add(argc,argv);
     }
     
-    struct stat st;
-
     return 0;
 }
