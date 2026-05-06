57c11ff83714f8203b55404b0fa1a6fef1d28305
Abhay Sachan <abhaysach05@gmail.com>
2026-05-06 13:00:39 +0000

    add index layout

---
+++ index.h
@@ -0,0 +1,22 @@
+#ifndef INDEX_H
+#define INDEX_H
+
+#include <stdint.h> // uint32_t, uint16_t
+
+typedef struct {
+    char signature[4];
+    uint32_t version; // int can differ across systems
+    uint32_t entry_count;
+} IndexHeader;
+
+typedef struct {
+    uint32_t mode;
+    unsigned char hash[20];
+    uint16_t path_len;
+    char *path;
+} IndexEntry;
+
+void update_index(const char *path, unsigned char hash[20]);
+
+#endif
+
